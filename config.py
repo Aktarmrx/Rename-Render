c0611f7f4fb1202c5549e24d9a42b5bda5e98548
Aktarmrx <ssk376860@gmail.com>
2024-12-01 16:26:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "29538599")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "b208ef895a7b942fb422db99633ad7a6")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7774420156:AAGVI4sTnIVf_xzDNrri1KpCcRVgUGR7U2mongodb+srv://ssk376860:AC1bWUYfaDZtNXFb@cluster0.wkmdl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
+mongodb+srv://ssk376860:AC1
+FORCE_SUB = os.environ.get("FORCE_SUB", "A_animex") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
@@ -21,13 +21,13 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz")
 
 DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://ssk376860:AC1bWUYfaDZtNXFb@cluster0.wkmdl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5565530322').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
